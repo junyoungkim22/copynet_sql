922f9aca8f9ea53a397b9f0f2cbf9fcddedc7aab
Junyoung Kim <junyoung2@gmail.com>
2019-12-09 16:45:46 +0900

    Add learning rate decay functionality

--- train.py
+++ train.py
@@ -15,7 +15,9 @@ from utils import to_np, trim_seqs
 
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
-from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction, sentence_bleu
+
+from torch.optim.lr_scheduler import StepLR
 
 
 def train(encoder_decoder: EncoderDecoder,
@@ -25,15 +27,26 @@ def train(encoder_decoder: EncoderDecoder,
           keep_prob,
           teacher_forcing_schedule,
           lr,
-          max_length):
+          max_length,
+          use_decay):
 
     global_step = 0
     loss_function = torch.nn.NLLLoss(ignore_index=0)
     optimizer = optim.Adam(encoder_decoder.parameters(), lr=lr)
     model_path = './saved/' + model_name + '/'
 
+    if(use_decay == False):
+        gamma = 1.0
+    else:
+        gamma = 0.5
+    scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+
+    #val_loss, val_bleu_score = evaluate(encoder_decoder, val_data_loader)
+
     for epoch, teacher_forcing in enumerate(teacher_forcing_schedule):
-        print('epoch %i' % epoch, flush=True)
+        #scheduler.step()
+        print('epoch %i' % (epoch), flush=True)
+        print('lr: ' + str(scheduler.get_lr()))
 
         for batch_idx, (input_idxs, target_idxs, input_tokens, target_tokens) in enumerate(tqdm(train_data_loader)):
             # input_idxs and target_idxs have dim (batch_size x max_len)
@@ -127,8 +140,10 @@ def train(encoder_decoder: EncoderDecoder,
 
         print('-' * 100, flush=True)
 
+        scheduler.step()
 
-def main(model_name, use_cuda, batch_size, teacher_forcing_schedule, keep_prob, val_size, lr, decoder_type, vocab_limit, hidden_size, embedding_size, max_length, seed=42):
+
+def main(model_name, use_cuda, batch_size, teacher_forcing_schedule, keep_prob, val_size, lr, decoder_type, vocab_limit, hidden_size, embedding_size, max_length, data_path, use_decay, saved_model_path, seed=42):
 
     model_path = './saved/' + model_name + '/'
 
@@ -141,17 +156,20 @@ def main(model_name, use_cuda, batch_size, teacher_forcing_schedule, keep_prob, 
     if os.path.isdir(model_path):
 
         print("loading encoder and decoder from model_path", flush=True)
-        encoder_decoder = torch.load(model_path + model_name + '.pt')
+        #encoder_decoder = torch.load(model_path + model_name + '.pt')
+        encoder_decoder = torch.load(saved_model_path)
 
         print("creating training and validation datasets with saved languages", flush=True)
-        train_dataset = SequencePairDataset(maxlen=max_length,
+        train_dataset = SequencePairDataset(data_path=data_path,
+                                            maxlen=max_length,
                                             lang=encoder_decoder.lang,
                                             use_cuda=use_cuda,
                                             val_size=val_size,
                                             use_extended_vocab=(encoder_decoder.decoder_type=='copy'),
                                             data_type='train')
 
-        val_dataset = SequencePairDataset(maxlen=max_length,
+        val_dataset = SequencePairDataset(data_path=data_path,
+                                          maxlen=max_length,
                                           lang=encoder_decoder.lang,
                                           use_cuda=use_cuda,
                                           val_size=val_size,
@@ -162,7 +180,8 @@ def main(model_name, use_cuda, batch_size, teacher_forcing_schedule, keep_prob, 
         os.mkdir(model_path)
 
         print("creating training and validation datasets", flush=True)
-        train_dataset = SequencePairDataset(maxlen=max_length,
+        train_dataset = SequencePairDataset(data_path=data_path,
+                                            maxlen=max_length,
                                             vocab_limit=vocab_limit,
                                             use_cuda=use_cuda,
                                             val_size=val_size,
@@ -170,7 +189,8 @@ def main(model_name, use_cuda, batch_size, teacher_forcing_schedule, keep_prob, 
                                             use_extended_vocab=(decoder_type=='copy'),
                                             data_type='train')
 
-        val_dataset = SequencePairDataset(maxlen=max_length,
+        val_dataset = SequencePairDataset(data_path=data_path,
+                                          maxlen=max_length,
                                           lang=train_dataset.lang,
                                           use_cuda=use_cuda,
                                           val_size=val_size,
@@ -202,7 +222,8 @@ def main(model_name, use_cuda, batch_size, teacher_forcing_schedule, keep_prob, 
           keep_prob,
           teacher_forcing_schedule,
           lr,
-          encoder_decoder.decoder.max_length)
+          encoder_decoder.decoder.max_length,
+          use_decay)
 
 
 if __name__ == '__main__':
@@ -210,6 +231,10 @@ if __name__ == '__main__':
     parser.add_argument('model_name', type=str,
                         help='the name of a subdirectory of ./model/ that '
                              'contains encoder and decoder model files')
+
+    parser.add_argument('--data_path', type=str)
+    parser.add_argument('--lr_decay', default='True', action='store_false')
+    parser.add_argument('--saved_model_path', type=str)
 
     parser.add_argument('--epochs', type=int, default=50,
                         help='the number of epochs to train')
@@ -220,14 +245,14 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=32,
                         help='number of examples in a batch')
 
-    parser.add_argument('--teacher_forcing_fraction', type=float, default=0.5,
+    parser.add_argument('--teacher_forcing_fraction', type=float, default=1,
                         help='fraction of batches that will use teacher forcing during training')
 
     parser.add_argument('--scheduled_teacher_forcing', action='store_true',
                         help='Linearly decrease the teacher forcing fraction '
                              'from 1.0 to 0.0 over the specified number of epocs')
 
-    parser.add_argument('--keep_prob', type=float, default=1.0,
+    parser.add_argument('--keep_prob', type=float, default=0.5,
                         help='Probablity of keeping an element in the dropout step.')
 
     parser.add_argument('--val_size', type=float, default=0.1,
@@ -244,7 +269,7 @@ if __name__ == '__main__':
                              'will be truncated to the most frequently'
                              'occurring words in the training dataset.')
 
-    parser.add_argument('--hidden_size', type=int, default=256,
+    parser.add_argument('--hidden_size', type=int, default=300,
                         help='The number of RNN units in the encoder. 2x this '
                              'number of RNN units will be used in the decoder')
 
@@ -262,5 +287,5 @@ if __name__ == '__main__':
     else:
         schedule = np.ones(args.epochs) * args.teacher_forcing_fraction
 
-    main(args.model_name, True, args.batch_size, schedule, args.keep_prob, args.val_size, args.lr, args.decoder_type, args.vocab_limit, args.hidden_size, args.embedding_size, args.max_length)
+    main(args.model_name, True, args.batch_size, schedule, args.keep_prob, args.val_size, args.lr, args.decoder_type, args.vocab_limit, args.hidden_size, args.embedding_size, args.max_length, args.data_path, args.lr_decay, args.saved_model_path)
     # main(str(int(time.time())), args.use_cuda, args.batch_size, schedule, args.keep_prob, args.val_size, args.lr, args.decoder_type, args.vocab_limit, args.hidden_size, args.embedding_size, args.max_length)
